7141e08f75d347ff37b65663026cf602a91937e4
Kimberly Aguilar Hermoso <kimaguilar2017@gmail.com>
2022-02-24 18:16:41 -0800

    updated properties

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,11 +1,10 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://0.0.0.0:27017/googlebooks', {
-  //These sections are not needed for mongose version 6 -> it is already implied
-  //useNewUrlParser: true,
-  //useUnifiedTopology: true,
-  //useCreateIndex: true,
- // useFindAndModify: false,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
 });
 
 module.exports = mongoose.connection;
